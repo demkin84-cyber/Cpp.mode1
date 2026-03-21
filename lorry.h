b051d0975e5875adfbde854265c7dd5a2a5e6020
Alex Demkin <demkin84@gmail.com>
2026-03-21 00:26:37 -0700

    Add Person class

---
+++ lorry.h
@@ -0,0 +1,25 @@
+#ifndef LORRY_H
+#define LORRY_H
+
+#include "Car.h"
+
+class Lorry : public Car {
+private:
+    int load_capacity;
+
+public:
+    Lorry();
+    Lorry(std::string b, int c, int p, int l);
+    Lorry(const Lorry& other);
+    ~Lorry();
+
+    int getLoadCapacity() const;
+    void setLoadCapacity(int l);
+
+    Lorry& operator=(const Lorry& other);
+
+    friend std::ostream& operator<<(std::ostream& out, const Lorry& lorry);
+    friend std::istream& operator>>(std::istream& in, Lorry& lorry);
+};
+
+#endif
